f8754034a1a41e6e0e8ea6853d8c4c3bb9909fb9
Nicole <82070537+nicausti@users.noreply.github.com>
2021-04-16 22:53:19 -0700

    Wk 3-2

--- Code.js
+++ Code.js
@@ -54,55 +54,83 @@ function countDown() {
         count = count - 1;
     }, 10000);
 }
-function countDownTimerV2(){
+function countDownTimerV2() {
     var count = 10;
-    for(var i = 1; i <= 10; i++) { 
-    setTimeout(function(){
-    document.getElementById("countDownTimer").innerHTML = count;
-    count--;}, 1000* i);
+    for (var i = 1; i <= 10; i++) {
+        setTimeout(function () {
+            document.getElementById("countDownTimer").innerHTML = count;
+            count--;
+        }, 1000 * i);
 
-} 
-setTimeout(function(){
-document.getElementById("countDownTimer").innerHTML = "Blastoff!";
-count--;}, 11000);}
+    }
+    setTimeout(function () {
+        document.getElementById("countDownTimer").innerHTML = "Blastoff!";
+        count--;
+    }, 11000);
+}
 
 //Creating the function to play Craps
-function playCraps(){
+function playCraps() {
     //Creating the variables for die1, die2, and the sum
-    var die1; 
+    var die1;
     var die2;
     var sum;
     //Random number generator, multiplied by 6 and with a ceiling
-    die1= Math.ceil(Math.random()*6);
-    die2= Math.ceil(Math.random()*6);
+    die1 = Math.ceil(Math.random() * 6);
+    die2 = Math.ceil(Math.random() * 6);
     //Notating the sum
-    sum= die1 + die2;
+    sum = die1 + die2;
     //To display results
     document.getElementById("die1Res").innerHTML = die1;
     document.getElementById("die2Res").innerHTML = die2;
     document.getElementById("sumRes").innerHTML = sum;
     //Generating rules of Craps game
-    if(sum == 7 || sum == 11){
-    document.getElementById("crapsResults").innerHTML = "Craps! You Lose!";}
-    
-    else if(die1 == die2 && die1%2 == 0){
-    document.getElementById("crapsResults").innerHTML = "Doubles! You Win!";}
-    
-    else document.getElementById("crapsResults").innerHTML = "Push. Please Try Again.";
-    }
-    
-    //Sandbox for creating loop timer
-    function runSandbox(){
-        var count = 10;
-        for(var i = 1; i <= 10; i++) { 
-        setTimeout(function(){
-        document.getElementById("sandboxDisplay").innerHTML = count;
-        count--;}, 1000* i);
-    
-    } 
-    setTimeout(function(){
-    document.getElementById("sandboxDisplay").innerHTML = "Blastoff!";
-    count--;}, 11000);
+    if (sum == 7 || sum == 11) {
+        document.getElementById("crapsResults").innerHTML = "Craps! You Lose!";
     }
 
-    
+    else if (die1 == die2 && die1 % 2 == 0) {
+        document.getElementById("crapsResults").innerHTML = "Doubles! You Win!";
+    }
+
+    else document.getElementById("crapsResults").innerHTML = "Push. Please Try Again.";
+}
+
+//Sandbox for creating loop timer
+function runSandbox() {
+    var count = 10;
+    for (var i = 1; i <= 10; i++) {
+        setTimeout(function () {
+            document.getElementById("sandboxDisplay").innerHTML = count;
+            count--;
+        }, 1000 * i);
+
+    }
+    setTimeout(function () {
+        document.getElementById("sandboxDisplay").innerHTML = "Blastoff!";
+        count--;
+    }, 11000);
+}
+//Countdown with change order request
+function countDownTimerV3() {
+    var count = 10;
+    for (var i = 1; i <= 10; i++) {
+        if (i <= 5) setTimeout(function () {
+            document.getElementById("countDownTimer").innerHTML = count;
+            count--;
+        }, 1000 * i);
+        else if (i == 11) setTimeout(function () {
+            document.getElementById("countDownTimer").innerHTML = "Blastoff!";
+            count--;
+        }, 1000 * i);
+        else setTimeout(function () {
+            document.getElementById("countDownTimer").innerHTML = "Warning! Less than halfway to launch. Time left= " + count;
+            count--;
+        }, 1000 * i);
+
+    }
+    setTimeout(function () {
+        document.getElementById("countDownTimer").innerHTML = "Blastoff!";
+        count--;
+    }, 11000);
+}
